a47c9c0ac4c3af86ac02a9e87f01454934943733
chellean-1120 <pedralbarochelleann.31@gmail.com>
2025-10-07 12:51:24 +0800

    Add password encryption

--- backend/src/main/java/com/ciel/blog/user/UserService.java
+++ backend/src/main/java/com/ciel/blog/user/UserService.java
@@ -1,6 +1,7 @@
 package com.ciel.blog.user;
 
 import com.ciel.blog.exception.InvalidCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,15 +9,18 @@ public class UserService {
     private final UserRepository userRepository;
     private final SignupMapper signupMapper;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository,  SignupMapper signupMapper,  JwtService jwtService) {
+    public UserService(UserRepository userRepository,  SignupMapper signupMapper,  JwtService jwtService,  PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.signupMapper = signupMapper;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public SignupResponseDto createUser(SignupDto dto) {
         User user = signupMapper.toUser(dto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User createdUser = userRepository.save(user);
         return signupMapper.toSignupResponseDto(createdUser);
     }
