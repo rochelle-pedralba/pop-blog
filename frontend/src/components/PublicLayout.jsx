9b27a4abda14a6bba16df7a651b1ce178df1012c
chellean-1120 <pedralbarochelleann.31@gmail.com>
2025-10-24 11:50:07 +0800

    Add feature post

--- frontend/src/components/PublicLayout.jsx
+++ frontend/src/components/PublicLayout.jsx
@@ -1,12 +1,33 @@
 import PublicHeader from "./PublicHeader.jsx";
+import image from "../assets/suga.jpg"
+import user from "../assets/user.svg"
+import time from "../assets/time.png"
 
 export  default function PublicLayout() {
     return (
         <>
             <PublicHeader/>
-            <h1>
-                PublicLayout
-            </h1>
+            <div className="feature-post">
+                <div className="overview">
+                    <h4>Featured Post</h4>
+                    <h1>BTS Suga Routine when composing songs</h1>
+                    <h2>Know more about Suga</h2>
+                    <div className="details">
+                        <div className="author">
+                            <img src={user} alt="user" />
+                            <p>Rochelle Ann</p>
+                        </div>
+                        <div className="date-posted">
+                            <img src={time} alt="time" />
+                            <p>8 min read</p>
+                        </div>
+                        <p>
+                            Oct 20, 2025
+                        </p>
+                    </div>
+                </div>
+                <img src={image} />
+            </div>
         </>
     )
 }
