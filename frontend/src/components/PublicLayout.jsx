908cfb0fdef184770d583046b11af53de202370b
chellean-1120 <pedralbarochelleann.31@gmail.com>
2025-10-24 17:17:21 +0800

    Added recent posts section

--- frontend/src/components/PublicLayout.jsx
+++ frontend/src/components/PublicLayout.jsx
@@ -1,11 +1,13 @@
 import PublicHeader from "./PublicHeader.jsx";
 import FeaturedPost from "./FeaturedPost.jsx"
+import RecentPost from "./RecentPost.jsx"
 
 export  default function PublicLayout() {
     return (
         <>
             <PublicHeader/>
             <FeaturedPost/>
+            <RecentPost/>
         </>
     )
 }
---
+++ frontend/src/components/RecentPost.jsx
@@ -0,0 +1,15 @@
+export default function RecentPost() {
+    return (
+        <div className="recent-articles">
+            <section className="header">
+                <div className="details">
+                    <h2>Recent Articles</h2>
+                    <h3>Stay updated on Pop Music with our latest posts!</h3>
+                </div>
+                <button>
+                    View All Articles
+                </button>
+            </section>
+        </div>
+    )
+}
