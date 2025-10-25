b08b21e4256735ca2e9b47a95aade44604ae16a4
chellean-1120 <pedralbarochelleann.31@gmail.com>
2025-10-25 14:56:17 +0800

    Add recent articles card

--- frontend/src/components/RecentPost.jsx
+++ frontend/src/components/RecentPost.jsx
@@ -1,4 +1,34 @@
+import cardData from '../cardData.json'
+import user from '../assets/user.svg'
+import time from '../assets/time.png'
+
 export default function RecentPost() {
+
+    const cardElements = cardData.map((card, index) =>
+        <div key={index} className="card">
+            <div className='image-container'>
+                <p className='category'>{card.category}</p>
+                <img src={card.imageUrl} alt={card.category + " picture"} />
+            </div>
+
+            <div className='description'>
+                <h3>{card.title}</h3>
+                <h4>{card.shortDescription}</h4>
+            </div>
+
+            <div className='details'>
+                <div className='author'>
+                    <img src={user} />
+                    <h5>{card.author}</h5>
+                </div>
+                <div className='reading-time'>
+                    <img src={time} />
+                    <h5>{card.readingTime}</h5>
+                </div>
+                <h5 className='published-date'>{card.datePublished}</h5>
+            </div>
+        </div>)
+
     return (
         <div className="recent-articles">
             <section className="header">
@@ -9,6 +39,9 @@ export default function RecentPost() {
                 <button>
                     View All Articles
                 </button>
+            </section>
+            <section className="content">
+                {cardElements}
             </section>
         </div>
     )
